c863cef447adf5a36ef42ee383dc8e4a4bb0d7b1
Mohamed Ramadan <mohameda7maa@gmail.com>
2024-07-23 13:28:53 +0300

    complete project setup with authentication

---
+++ packages/db/prisma/migrations/20240723102117_change_user_schema/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `age` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "age",
+ALTER COLUMN "email" DROP NOT NULL,
+ALTER COLUMN "name" DROP NOT NULL;
